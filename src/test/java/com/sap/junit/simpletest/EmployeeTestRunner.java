ab2982c6fca2c0df96709b8c718d3679c4fb5461
I047711 <silviya.brayanova@sap.com>
2016-08-15 15:23:57 +0300

    Runner example

---
+++ src/test/java/com/sap/junit/simpletest/EmployeeTestRunner.java
@@ -0,0 +1,17 @@
+package com.sap.junit.simpletest;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+ 
+public class EmployeeTestRunner {
+	public static void main(String[] args) {
+	      Result result = JUnitCore.runClasses(EmployeeDetailsTest.class);
+			
+	      for (Failure failure : result.getFailures()) {
+	         System.out.println(failure.toString());
+	      }
+			
+	      System.out.println(result.wasSuccessful());
+	   }
+}
